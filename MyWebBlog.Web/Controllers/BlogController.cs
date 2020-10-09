18385730e85ed7a924eb1a5c99fe87d3af76a029
YoonkunLee <ydx9474@aut.ac.nz>
2020-10-09 17:52:36 +1300

    Created layer

---
+++ MyWebBlog.Web/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebBlog.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateBlog()
+        {
+            return View();
+        }
+
+        public IActionResult GoToCreateBlog()
+        {
+            return RedirectToPage("CreactBolg");
+        }
+    }
+}
---
+++ MyWebBlog/ViewModel/blog.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWebBlog.Model
+{
+    class blog
+    {
+        Guid Id { get; set; }
+        Date date { get; set; }
+        String text { get; set; }
+    }
+}
